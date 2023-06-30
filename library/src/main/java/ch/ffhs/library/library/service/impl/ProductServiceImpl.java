a312a0caa5c7ae1de2b64307173a62722540d8c0
Naveen <naveen.anpalagan@students.ffhs.ch>
2023-06-30 12:28:58 +0200

    product name

--- library/src/main/java/ch/ffhs/library/library/service/impl/ProductServiceImpl.java
+++ library/src/main/java/ch/ffhs/library/library/service/impl/ProductServiceImpl.java
@@ -82,7 +82,7 @@ public class ProductServiceImpl implements ProductService {
             Product product = optionalProduct.get();
             ProductDto productDto = new ProductDto();
             productDto.setId(product.getId());
-            productDto.setName(productDto.getName());
+            productDto.setName(product.getName());
             productDto.setDescription(product.getDescription());
             productDto.setPrice(product.getPrice());
             productDto.setCurrentQuantity(product.getCurrentQuantity());
