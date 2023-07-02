07b70b434457e81687e6c2a8db437760d25e6f9a
Nina Aegerter <nina.aegerter@students.ffhs.ch>
2023-07-02 17:42:04 +0200

    changed and deleted some things

--- library/src/main/java/ch/ffhs/library/library/model/Customer.java
+++ library/src/main/java/ch/ffhs/library/library/model/Customer.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.List;
-
 /**
  * This class represents a customer
  */
@@ -16,7 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "customers", uniqueConstraints = @UniqueConstraint(columnNames = {"username", "email", "phone_number"}))
+@Table(name = "customers", uniqueConstraints = @UniqueConstraint(columnNames = {"username","email", "phone_number"}))
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,11 +34,11 @@ public class Customer {
     private String password;
 
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "city_id", referencedColumnName = "city_id")
+    @JoinColumn(name= "city_id", referencedColumnName = "city_id")
     private City city;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.MERGE})
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "customer_roles", joinColumns = @JoinColumn(name = "customer_id", referencedColumnName = "customer_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"))
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"))
     private Collection<Role> roles;
 }
